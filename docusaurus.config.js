34f8dff25f7b1309b91b25eefda07fec36f09dce
Abhishek Jain <ajj_2396@yahoo.com>
2024-07-30 22:35:49 +0530

    feat: Install Google Tag manager plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -37,6 +37,12 @@ const config = {
   presets: [
     [
       'classic',
+      {
+        gtag: {
+          trackingID: 'G-HVVG0G23PW',
+          anonymizeIP: false,
+        }
+      },
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
